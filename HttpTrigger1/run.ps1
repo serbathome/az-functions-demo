d7d3c8646ed7326c7b183759fab009d7688a2c34
Sergey Biryukov <sbiryukov@microsoft.com>
2025-09-23 22:21:42 +0200

    Refactor access token retrieval and pipeline upload functions for improved clarity and efficiency

--- HttpTrigger1/run.ps1
+++ HttpTrigger1/run.ps1
@@ -2,105 +2,49 @@ using namespace System.Net
 
 param($Request, $TriggerMetadata)
 
-
-function savePipelineToBlob {
-    param (
-        $pipelineJson,
-        $BlobName,
-        $accessToken
-    )
-    $StorageAccount = "bkpacc"
-    $Container      = "pipelines"
-    # REST endpoint
-    $Uri = "https://$StorageAccount.blob.core.windows.net/$Container/$BlobName"
-    # Encode body
-    $Body          = [System.Text.Encoding]::UTF8.GetBytes($pipelineJson)
-    # $ContentLength = $Body.Length
-    $Headers = @{
-        "Authorization" = "Bearer $accessToken"
-        "x-ms-version"  = "2021-12-02"
-        "x-ms-date"     = (Get-Date).ToUniversalTime().ToString("R") # RFC1123
-        "x-ms-blob-type"= "BlockBlob"
-        "Content-Type"  = "text/plain; charset=UTF-8"
-        }
-    $response = Invoke-RestMethod -Method PUT -Uri $Uri -Headers $Headers -Body $Body
-    Write-Host "Blob uploaded successfully: $Container/$BlobName"
-    return $response
-}
-
-
-$tenantId = $env:TENANT_ID
-$clientId = $env:CLIENT_ID
-$clientSecret = $env:SECRET
-$subscriptionId = $env:SUBSCRIPTION_ID
-$resourceGroup = $env:RESOURCE_GROUP
-$dataFactoryName = $env:DATA_FACTORY_NAME
-
-
-$tokenEndpoint = "https://login.microsoftonline.com/$tenantId/oauth2/token"
-$tokenParams = @{
-    "grant_type" = "client_credentials"
-    "client_id" = $clientId
-    "client_secret" = $clientSecret
-    "resource" = "https://management.core.windows.net"
-}
-
-# get access token from Azure AD using client credentials for Azure Resource Manager
-try {
-    # Request access token for Azure Resource Manager
-    $accessTokenResponse = Invoke-RestMethod -Uri $tokenEndpoint -Method POST -Body $tokenParams
-
-    # Extract access token
-    $accessToken = $accessTokenResponse.access_token
-    $response = "Access Token: $accessToken"
-}
-catch {
-    $response = "Failed to obtain access token: $_"
-}
-
-# get access token for Azure Storage
-$storageTokenParams = @{
-    "grant_type" = "client_credentials"
-    "client_id" = $clientId
-    "client_secret" = $clientSecret
-    "resource" = "https://storage.azure.com/"
-}
-
-try {
-    # Request access token for Storage
-    $storageTokenResponse = Invoke-RestMethod -Uri $tokenEndpoint -Method POST -Body $storageTokenParams
-    $storageAccessToken = $storageTokenResponse.access_token
-}
-catch {
-    Write-Output "Failed to obtain storage access token: $_"
-}
-
-# get list of data factory pipelines
-$headers = @{
-    'Authorization' = "Bearer $accessToken"
-    'Content-Type' = 'application/json'
-}
-$listUrl = "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.DataFactory/factories/$dataFactoryName/pipelines?api-version=2018-06-01"
-$pipelines = Invoke-RestMethod -Uri $listUrl -Method Get -Headers $headers
-
-# Export each pipeline JSON
-foreach ($pipeline in $pipelines.value) {
-    $pipelineName = $pipeline.name
-    Write-Output "Processing pipeline: $pipelineName"
-    # call blob REST API to upload JSON from $pipeline object
-    $pipelineJson = $pipeline | ConvertTo-Json -Depth 10
-    try {
-        savePipelineToBlob -pipelineJson $pipelineJson -BlobName $pipelineName -accessToken $storageAccessToken
+function Get-AccessToken($resource) {
+    $tokenParams = @{
+        grant_type = "client_credentials"
+        client_id = $env:CLIENT_ID
+        client_secret = $env:SECRET
+        resource = $resource
     }
-    catch {
-        Write-Output "Failed to upload pipeline $pipelineName : $_"
+    (Invoke-RestMethod -Uri "https://login.microsoftonline.com/$($env:TENANT_ID)/oauth2/token" -Method POST -Body $tokenParams).access_token
+}
+
+function Save-PipelineToBlob($pipelineJson, $blobName, $accessToken) {
+    $uri = "https://bkpacc.blob.core.windows.net/pipelines/$blobName"
+    $headers = @{
+        Authorization = "Bearer $accessToken"
+        "x-ms-version" = "2021-12-02"
+        "x-ms-date" = (Get-Date).ToUniversalTime().ToString("R")
+        "x-ms-blob-type" = "BlockBlob"
+        "Content-Type" = "text/plain; charset=UTF-8"
+    }
+    Invoke-RestMethod -Method PUT -Uri $uri -Headers $headers -Body ([System.Text.Encoding]::UTF8.GetBytes($pipelineJson))
+}
+
+try {
+    $managementToken = Get-AccessToken "https://management.core.windows.net"
+    $storageToken = Get-AccessToken "https://storage.azure.com/"
+    
+    $listUrl = "https://management.azure.com/subscriptions/$($env:SUBSCRIPTION_ID)/resourceGroups/$($env:RESOURCE_GROUP)/providers/Microsoft.DataFactory/factories/$($env:DATA_FACTORY_NAME)/pipelines?api-version=2018-06-01"
+    $pipelines = Invoke-RestMethod -Uri $listUrl -Headers @{ Authorization = "Bearer $managementToken" }
+    
+    foreach ($pipeline in $pipelines.value) {
+        Write-Output "Processing pipeline: $($pipeline.name)"
+        Save-PipelineToBlob ($pipeline | ConvertTo-Json -Depth 10) $pipeline.name $storageToken
     }
     
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = [HttpStatusCode]::OK
+        Body = "Pipelnes backup was succesfully completed."
+    })
 }
-
-
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::OK
-    Body       = $pipelines
-})
+catch {
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = [HttpStatusCode]::InternalServerError
+        Body = "Error: $_"
+    })
+}
 
