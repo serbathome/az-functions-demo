a9e38445d28bac89b0ea1b10b0935d89526e9a36
Sergey Biryukov <sbiryukov@microsoft.com>
2025-09-23 12:59:34 +0200

    Remove storage token retrieval and API call from HTTP trigger

--- HttpTrigger1/run.ps1
+++ HttpTrigger1/run.ps1
@@ -2,23 +2,7 @@ using namespace System.Net
 
 param($Request, $TriggerMetadata)
 
-# Request token for Storage
-$resource = "https://storage.azure.com/"
-$tokenResponse = Invoke-RestMethod -Method GET `
-    -Uri "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2019-08-01&resource=$resource" `
-    -Headers @{ Metadata = "true" }
-
-$bearerToken = $tokenResponse.access_token
-
-# Use token to call Storage REST API
-$storageAccount = "pipelinebkp"
-$headers = @{
-    Authorization = "Bearer $bearerToken"
-    "x-ms-version" = "2021-08-06"
-}
-
-$response = Invoke-RestMethod -Uri "https://$storageAccount.blob.core.windows.net/?comp=list" `
-    -Method GET -Headers $headers
+$response = "Hello, world! This HTTP triggered function executed successfully."
 
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
