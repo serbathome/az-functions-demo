37f54c082ab6d34d439455058f9c1ae2cee079e0
Sergey Biryukov <sbiryukov@microsoft.com>
2025-09-23 14:13:07 +0200

    Add token retrieval logic to HTTP trigger function

--- HttpTrigger1/run.ps1
+++ HttpTrigger1/run.ps1
@@ -4,6 +4,32 @@ param($Request, $TriggerMetadata)
 
 $response = "Hello, world! This HTTP triggered function executed successfully."
 
+
+$tenantId = $env:TENANT_ID
+$clientId = $env:CLIENT_ID
+$clientSecret = $env:SECRET
+
+$tokenEndpoint = "https://login.microsoftonline.com/$tenantId/oauth2/token"
+$tokenParams = @{
+    "grant_type" = "client_credentials"
+    "client_id" = $clientId
+    "client_secret" = $clientSecret
+    "resource" = "https://manage.microsoft.com"
+}
+
+try {
+    # Request access token
+    $accessTokenResponse = Invoke-RestMethod -Uri $tokenEndpoint -Method POST -Body $tokenParams
+
+    # Extract access token
+    $accessToken = $accessTokenResponse.access_token
+    $response = "Access Token: $accessToken"
+}
+catch {
+    $response = "Failed to obtain access token: $_"
+}
+
+
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
     Body       = $response
