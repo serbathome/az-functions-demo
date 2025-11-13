8cea0227775a905dc46ffeef54304c50e0dadc08
Sergey Biryukov <serbathome@gmail.com>
2025-11-13 12:41:43 +0100

    Add Save-DatasetToBlob function to handle dataset storage and update processing logic

--- HttpTrigger1/run.ps1
+++ HttpTrigger1/run.ps1
@@ -30,6 +30,18 @@ function Save-PipelineToBlob($pipelineJson, $pipelineName, $pipelineFolderName, 
     Invoke-RestMethod -Method PUT -Uri $uri -Headers $headers -Body ([System.Text.Encoding]::UTF8.GetBytes($pipelineJson))
 }
 
+function Save-DatasetToBlob($datasetJson, $datasetName, $datasetFolderName, $accessToken) {
+    $uri = "https://$storageAccountName.blob.core.windows.net/$containerName/$folderName/$datasetFolderName/$datasetName"
+    $headers = @{
+        Authorization = "Bearer $accessToken"
+        "x-ms-version" = "2021-12-02"
+        "x-ms-date" = (Get-Date).ToUniversalTime().ToString("R")
+        "x-ms-blob-type" = "BlockBlob"
+        "Content-Type" = "text/plain; charset=UTF-8"
+    }
+    Invoke-RestMethod -Method PUT -Uri $uri -Headers $headers -Body ([System.Text.Encoding]::UTF8.GetBytes($datasetJson))
+}
+
 try {
     $managementToken = Get-AccessToken "https://management.core.windows.net"
     $storageToken = Get-AccessToken "https://storage.azure.com/"
@@ -41,6 +53,16 @@ try {
         Write-Output "Processing pipeline: $($pipeline.name)"
         Write-Output "Saving pipeline in the folder: $($pipeline.properties.folder.name)"
         Save-PipelineToBlob ($pipeline | ConvertTo-Json -Depth 10) $pipeline.name $pipeline.properties.folder.name $storageToken
+    }
+
+    $datasetListUrl = "https://management.azure.com/subscriptions/$($env:SUBSCRIPTION_ID)/resourceGroups/$($env:RESOURCE_GROUP)/providers/Microsoft.DataFactory/factories/$factoryName/datasets?api-version=2018-06-01"
+   
+    $datasets = Invoke-RestMethod -Uri $datasetListUrl -Headers @{ Authorization = "Bearer $managementToken" }
+   
+    foreach ($dataset in $datasets.value) {
+        Write-Output "Processing dataset: $($dataset.name)"
+        Write-Output "Saving dataset in the folder: $($dataset.properties.folder.name)"
+        Save-DatasetToBlob ($dataset | ConvertTo-Json -Depth 10) $dataset.name $dataset.properties.folder.name $storageToken
     }
     
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
